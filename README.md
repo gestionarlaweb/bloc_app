872ade91b2907c08296e754ea14db9b7c1e69507
David Rabassa Planas <davidrabassaplanas@Air-de-David.home>
2022-06-28 21:39:31 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# bloc_app
+## Flutter Login con Bloc State Management
+## BLoc (Cubit)
+- Conexión a la Api de pruebas https://reqres.in/api
+- En email: eve.holt@reqres.in
+- En password: cityslicka
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:bloc_app/app/login/view/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: LoginView(),
+    );
+  }
+}
