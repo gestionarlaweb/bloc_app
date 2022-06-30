a4d2eebb822cf8992cc03ed6bb125b05f22c3d91
David Rabassa Planas <davidrabassaplanas@Air-de-David.home>
2022-06-30 23:18:39 +0200

    empezando detalle gif

---
+++ lib/app/login/view/widgets/view/details_gif.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailsGif extends StatelessWidget {
+  const DetailsGif({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Detalle Gif')),
+        body: Center(
+          child: Text('text'),
+        ));
+  }
+}
--- lib/app/login/view/widgets/view/list_gif.dart
+++ lib/app/login/view/widgets/view/list_gif.dart
@@ -9,6 +9,7 @@ class ListGif extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
+      scrollDirection: Axis.vertical,
       itemCount: gifs.length,
       itemBuilder: (BuildContext context, int index) {
         return _TargetGig(gif: gifs[index], index: index);
@@ -25,46 +26,45 @@ class _TargetGig extends StatelessWidget {
       : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 10),
-          margin: EdgeInsets.only(top: 10),
-          child: Wrap(
-            children: [
-              Center(
-                child: Text(
-                  '${gif.title}-',
-                  style: TextStyle(color: Colors.blueGrey),
-                ),
-              ),
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  child: Column(
-                    /*
-                    children: [
-                      Image.network(
-                          'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
-                    ],
-                    */
-
-                    children: [
-                      FadeInImage(
-                        placeholder: AssetImage('assets/img/giphy.gif'),
-                        image: NetworkImage(
-                            gif.images.original.url), // '${gif.url}.gif'
-                      ),
-                    ],
+  Widget build(BuildContext context) => OrientationBuilder(
+        builder: (context, orientation) {
+          //final isPortrait = orientation == Orientation.portrait;
+          // return GridView.count(
+          // crossAxisCount: isPortrait ? 1 : 3,
+          // children: List.generate(
+          //   20,
+          //   (index) => Column(
+          return SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              margin: EdgeInsets.only(top: 10),
+              child: Wrap(
+                children: [
+                  Center(
+                    child: Text(
+                      '${gif.title}-',
+                      style: TextStyle(color: Colors.blueGrey),
+                    ),
                   ),
-                ),
+                  Center(
+                    child: Container(
+                      height: 120,
+                      width: 120,
+                      child: Column(
+                        children: [
+                          FadeInImage(
+                            placeholder: AssetImage('assets/img/giphy.gif'),
+                            image: NetworkImage(
+                                gif.images.original.url), // '${gif.url}.gif'
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
+            ),
+          );
+        },
+      );
 }
