900935ae9aa6144223335214e0f5b0dae079714c
David Rabassa Planas <davidrabassaplanas@Air-de-David.home>
2022-07-01 20:01:56 +0200

    animations

--- lib/app/login/view/widgets/view/details_gif.dart
+++ lib/app/login/view/widgets/view/details_gif.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_app/app/login/model/gif_model.dart';
 import 'package:bloc_app/core/constants/app.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 
 class DetailsGif extends StatelessWidget {
   const DetailsGif({Key? key}) : super(key: key);
@@ -14,27 +15,63 @@ class DetailsGif extends StatelessWidget {
           title: Text('Detalle Gif'),
           backgroundColor: AppConstants.kColorAppBar,
         ),
-        body: Container(
-          padding: EdgeInsets.only(bottom: 10),
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(gif.id),
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(gif.title),
-              ),
-              Container(
-                padding: EdgeInsets.only(bottom: 10),
-                child: Text(gif.embedUrl),
-              ),
-              FadeInImage(
-                  placeholder: AssetImage('assets/giphy.gif'),
-                  image: NetworkImage(gif.images.original.url))
-            ],
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.only(bottom: 10),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Id',
+                        style: textFontDetail(),
+                      ),
+                      FadeInDown(
+                          delay: Duration(milliseconds: 2000),
+                          child: Text(gif.id)),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Title',
+                        style: textFontDetail(),
+                      ),
+                      FadeInLeft(
+                          delay: Duration(milliseconds: 2500),
+                          child: Text(gif.title)),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Column(
+                    children: [
+                      Text(
+                        'URL',
+                        style: textFontDetail(),
+                      ),
+                      FadeInRight(
+                          delay: Duration(milliseconds: 3000),
+                          child: Text(gif.embedUrl)),
+                    ],
+                  ),
+                ),
+                FadeInImage(
+                    placeholder: AssetImage('assets/giphy.gif'),
+                    image: NetworkImage(gif.images.original.url))
+              ],
+            ),
           ),
         ));
+  }
+
+  TextStyle textFontDetail() {
+    return TextStyle(fontSize: 16, fontWeight: FontWeight.w200);
   }
 }
